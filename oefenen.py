a7a502573823967d57e0082aad5c6e7c685626af
sannepost2001 <43266412+sannepost2001@users.noreply.github.com>
2019-06-03 11:33:20 +0200

    Werkt
    Signed-off-by: sannepost2001 <43266412+sannepost2001@users.noreply.github.com>

--- oefenen.py
+++ oefenen.py
@@ -1,90 +1,139 @@
-# Maite van den Noort, bin-1a
-# 22-05-2019
-# applicatie om sql database te doorzoeken op een twee zoekwoorden,
-# retoureneert ja of nee
-import mysql.connector
+app = Flask(__name__)
 
 
-def main():
-    '''De main laat twee zoekwoorden invullen door de gebruiker, roept dan de
-     twee functies zoeken en zoeken2. Die gaan met behulp van query zoeken in
-      de database naar dat zoekwoord en stoppen het in records
+@app.route('/')
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def pagina(filename):
+    if filename == "bacterie.html":
+        # """ Als er op zoeken op bacterie naam wordt gebruikt dan wordt de bacterie.html pagina aangeroepen.
+        # deze pagina moet nog aangevuld worden met de data die je kruigt als je filterd op het zoekword
+        # """
 
-    input: twee zoekwoorden
-    output: '''
-    conn = mysql.connector.connect(
-        host="ensembldb.ensembl.org", user="anonymous",
-        db='ensembl_production_95')
-    zoekwoord1 = input('geef een zoekwoord op: ')
-    zoekwoord2 = input('geef een tweede zoekwoord op:')
-    records = zoeken(zoekwoord1)
-    records2 = zoeken2(zoekwoord2)
-    zelfde(records, records2)
+        searchword = ""
+        zoekwoord = ""
+        teruggeven = ""
+        giveback = ""
+        if request.method == 'POST':
+            conn = mysql.connector.connect(host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com",
+                                           user="kxxxf@hannl-hlo-bioinformatica-mysqlsrv", db="kxxxf",
+                                           password="ConnectionPWD")
+            cursor = conn.cursor()
 
+            try:
+                zoekwoord = request.form["zoekwoord"]
+            except KeyError:
+                zoekwoord = ""
 
-def zoeken(zoekwoord1):
-    conn = mysql.connector.connect(
-        host="ensembldb.ensembl.org", user="anonymous",
-        db='ensembl_production_95')
-    cursor = conn.cursor()
-    query1 = "select a.alias, s.common_name, s.scientific_name from species s join species_alias a on a.species_id = s.species_id where a.alias like '%" + zoekwoord1 + "%' or s.common_name like '%" + zoekwoord1 + "%' or s.scientific_name like '%" + zoekwoord1 + "%'"
-    cursor.execute(query1)
-    records = cursor.fetchall()
-    cursor.close()
-    print(records)
-    return records
+            try:
+                searchword = request.form["searchword"]
+            except KeyError:
+                searchword = ""
 
+            if zoekwoord != "":
+                # Query die zoekt op het zoekwoord
+                cursor.execute("""select blast.name, blast.accessioncode, functionality.function from blast join functionint
+                                  on blast.id = functionint.BLAST_id join functionality on functionint.functionality_id = 
+                                  functionality.id where blast.name like '%" + zoekwoord + "%'""")
+                records = cursor.fetchall()  # lijst met al de namen die het zoekwoord in de naam hebben
 
-def zoeken2(zoekwoord2):
-    conn = mysql.connector.connect(
-        host="ensembldb.ensembl.org", user="anonymous",
-        db='ensembl_production_95')
-    cursor = conn.cursor()
-    query2 = "select a.alias, s.common_name, s.scientific_name from species s join species_alias a on a.species_id = s.species_id where a.alias like '%" + zoekwoord2 + "%' or s.common_name like '%" + zoekwoord2 + "%' or s.scientific_name like '%" + zoekwoord2 + "%'"
-    cursor.execute(query2)
-    records2 = cursor.fetchall()
-    cursor.close()
-    print(records2)
-    return records2
+                teruggeven = ("<p2>Gevonden data van het zoeken op protiën naam:</p2><br>\n"
+                              + "<table id=\"myTable\" style=\"width:777px; height: 400px;\">"
+                              + "   <tr>\n"
+                              + "   <th onclick=\"sortTable(0)\">Naam</th>\n"
+                              + "   <th onclick=\"sortTable(1)\">Accessiecode:</th>\n"
+                              + "   <th onclick=\"sortTable(2)\">Functie:</th>\n"
+                              + "   </tr>")
+                for rows in records:
+                    for row in rows:
+                        teruggeven = teruggeven + "<tr>"
+                        teruggeven = teruggeven + "<td>" + (row[0]) + "</td>"
+                        teruggeven = teruggeven + "<td>" + (row[1]) + "</td>"
+                        teruggeven = teruggeven + "<td>" + (row[2]) + "</td>"
+                        teruggeven = teruggeven + "</tr>"
+                teruggeven = teruggeven + "</table>"
+            if searchword != "":
+                cursor.execute("""select taxonomy.name, blast.name from taxonomy join blast on blast.TAXONOMY_id = 
+                                              taxonomy.id join taxonomy b on b.TAXONOMY_id = taxonomy.id where taxonomy.name like 
+                                              '%" + searchword + "%'""")
+                data = cursor.fetchall()
+                cursor.close()
+                conn.close()
+                # maakt een tabel van de gevonden data
+                giveback = ("<p2>Gevonden data van het zoeken op taxonomy:</p2><br>\n"
+                            + "<table id=\"myTable\" style=\"width:777px; height: 400px;\">"
+                            + "   <tr>\n"
+                            + "   <th onclick=\"sortTable(0)\">Naam</th>\n"
+                            + "   <th onclick=\"sortTable(1)\">Taxonomy</th>\n"
+                            + "   </tr>")
 
+                for a in data:
+                    for i in a:
+                        giveback = giveback + "<tr>"
+                        giveback = giveback + "<td>" + (i[0]) + "</td>"
+                        giveback = giveback + "<td>" + (i[1, 2]) + "</td>"
+                        giveback = giveback + "</tr>"
 
-def zelfde(records, records2):
-    org1 = []
-    org2 = []
-    org3 = []
-    orga = []
-    orgb = []
-    orgc = []
+                giveback = giveback + "</table>"
 
-    for row in records:
-        org1 = row[0]
-        org2 = row[1]
-        org3 = row[2]
-    for row in records2:
-        orga = row[0]
-        orgb = row[1]
-        orgc = row[2]
-    for row in records:
-        if org1.find(orga) == 1:
-            print('Het zijn gelijke organismen')
-        elif org1 == orgb:
-            print('Het zijn gelijke organismen')
-        elif org1 == orgc:
-            print('Het zijn gelijke organismen')
-        elif org2 == orga:
-            print('Het zijn gelijke organismen')
-        elif org2 == orgb:
-            print('Het zijn gelijke organismen')
-        elif org2 == orgc:
-            print('Het zijn gelijke organismen')
-        elif org3 == orga:
-            print('Het zijn gelijke organismen')
-        elif org3 == orgb:
-            print('Het zijn gelijke organismen')
-        elif org3 == orgb:
-            print('Het zijn gelijke organismen')
-        else:
-            print('het zijn niet gelijke organismen')
+            return render_template("bacterie.html", teruggeven=teruggeven, zoekwoord=zoekwoord, giveback=giveback,
+                                   searchword=searchword)
 
-
-main()
+    elif filename == "tabel.html":
+        """"""
+        # try:
+        teruggeven = ""
+        sql = ""
+        naam = ""
+        coverage = ""
+        score = ""
+        identity = ""
+        evalue = ""
+        if request.method == 'POST':
+            conn = mysql.connector.connect(host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com",
+                                           user="kxxxf@hannl-hlo-bioinformatica-mysqlsrv", db="kxxxf",
+                                           password="ConnectionPWD")
+            cursor = conn.cursor()
+            naam = request.form["naam"]
+            coverage = request.form["coverage"]
+            score = request.form["score"]
+            identity = request.form["identity"]
+            evalue = request.form["evalue"]
+            if naam != "":
+                #
+                sql = sql + " and blast.name like '%" + naam + "%'"
+            if coverage != "":
+                #
+                sql = sql + " and querycoverage > " + coverage
+            if score != "":
+                #
+                sql = sql + " and sequence.score > " + score
+            if identity != "":
+                #
+                sql = sql + " and percidentity > " + identity
+            if evalue != "":
+                #
+                sql = sql + " and evalue < " + evalue + "%'"
+            #
+            cursor.execute("select blast.name, evalue, bits, querycoverage, percidentity from blast where 1=1" + sql)
+            records = cursor.fetchall()  # lijst met al de namen die het zoekwoord in de naam hebben
+            cursor.close()
+            conn.close()
+            teruggeven = ("<table id=\"myTable\" style=\"width:777px; height: 400px;\">"
+                          + "    <tr>\n"
+                          + "    <th onclick=\"sortTable(0)\">Naam</th>\n"
+                          + "    <th onclick=\"sortTable(1)\">E-value:</th>\n"
+                          + "    <th onclick=\"sortTable(2)\">(Bit)score:</th>\n"
+                          + "    <th onclick=\"sortTable(3)\">Coverage:</th>\n"
+                          + "    <th onclick=\"sortTable(4)\">%identity:</th>\n"
+                          + "    </tr>")
+            for row in records:
+                teruggeven = teruggeven + "<tr>"
+                teruggeven = teruggeven + "<td>" + str((row[0])) + "</td>"
+                teruggeven = teruggeven + "<td>" + str((row[1])) + "</td>"
+                teruggeven = teruggeven + "<td>" + str((row[2])) + "</td>"
+                teruggeven = teruggeven + "<td>" + str((row[3])) + "</td>"
+                teruggeven = teruggeven + "<td>" + str((row[4])) + "</td>"
+                teruggeven = teruggeven + "</tr>"
+            teruggeven = teruggeven + "</table>"
+        return (render_template("tabel.html", teruggeven=teruggeven, naam=naam, identity=identity, score=score,
+                                evalue=evalue, coverage=coverage))
